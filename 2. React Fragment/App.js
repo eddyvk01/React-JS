1b453f4e20cefef27983dab4c911c1bd728d2e83
vineet <vineet123@gmail.com>
2022-12-09 14:00:48 +0530

    React Fragment

---
+++ 2. React Fragment/App.js
@@ -0,0 +1,19 @@
+import React, { Component, Fragment } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      // <Fragment>
+      //   <h1>Hello GeekyShows</h1>
+      //   <h2>Hello World</h2>
+      // </Fragment>
+      // Short-hand method for fragment
+      <>
+        <h1>Hello GeekyShows</h1>
+        <h2>Hello World</h2>
+      </>
+    );
+  }
+}
+
+export default App;
